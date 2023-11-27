fc4d16f5810b66b32f850361ad67b5f456bc2c07
GasiliiGL <vasiliiglavatskih@gmail.com>
2023-11-27 20:44:37 +0400

    instal

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\AdminUser;
+use App\Models\Post;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+         \App\Models\User::factory(10)->create();
+
+         Post::factory(10)->create();
+
+         AdminUser::factory(1)->create([
+            "name"=>"Admin",
+            "email"=>"ordermanager@ordermanager.com",
+            "password"=>bcrypt("12345"),
+
+         ]);
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
