083045e947640963dab23464d1764a96f26d521a
GasiliiGL <vasiliiglavatskih@gmail.com>
2023-12-02 15:09:50 +0400

    выпоним наследование от одного шаблона, определим секции контента страницы

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,1 +1,5 @@
+@extends ('layouts.app')
+
+@section('content')
 <h1> Контакты</h1>
+@endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,12 @@
-<h1> Главная страница</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+		<title>Editorial by HTML5 UP</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+<body>
+    <h1> Главная страница</h1>
+</body>
+</html>
