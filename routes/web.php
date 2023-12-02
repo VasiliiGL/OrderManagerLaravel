4c85399e5a94446056af9ed109418aac27422371
GasiliiGL <vasiliiglavatskih@gmail.com>
2023-12-02 14:22:28 +0400

    обновление, исправлен путь

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
-Route::get('/', function () {
+Route::get('/about', function () {
     return view('about');
 });
-Route::get('/', function () {
+Route::get('/contact', function () {
     return view('contact');
 });
 
