facd7f986d47817698bb9779629d6a7bf592d980
GasiliiGL <vasiliiglavatskih@gmail.com>
2023-12-04 20:14:30 +0400

    Раздел контакты

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -244,14 +244,16 @@
 			<!-- Section -->
 				<section>
 					<header class="major">
-						<h2>Get in touch</h2>
+						<h2>Связаться</h2>
 					</header>
-					<p>Sed varius enim lorem ullamcorper dolore aliquam aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore. Proin sed aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aliquam.</p>
+					<p>Контакт-центр АО «ИжПЗ» осуществляет консультации по продукции и услугам группы компаний АО «ИжПЗ», а также обработку жалоб, отзывов и предложений.</p>
 					<ul class="contact">
 						<li class="icon solid fa-envelope"><a href="#">information@untitled.tld</a></li>
 						<li class="icon solid fa-phone">(000) 000-0000</li>
 						<li class="icon solid fa-home">1234 Somewhere Road #8254<br />
 						Nashville, TN 00000-0000</li>
+						<li class="icon solid fa-home">Горячая линия безопасности АО «ИжПЗ»<br />
+						Электронная почта: doverie@izpz.ru</li>
 					</ul>
 				</section>
 
