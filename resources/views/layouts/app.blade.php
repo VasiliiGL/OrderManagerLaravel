e64c2b3bd750c09b4a7a469ede6995933824bff7
GasiliiGL <vasiliiglavatskih@gmail.com>
2023-12-04 19:52:04 +0400

    Подбор изображений компаний

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -113,7 +113,7 @@
 					</header>
 					<div class="posts">
 						<article>
-							<a href="#" class="image"><img src="images/pic01.jpg" alt="" /></a>
+							<a href="#" class="image"><img src="images/ОООИжПЗ.jpg" alt="" /></a>
 							<h3>ООО ИжПЗ</h3>
 							<p>Бортовые и наземные радиотехнические комплексы, телеметрические системы, системы визуального контроля, системы связи, робототехника.</p>
 							<ul class="actions">
@@ -121,7 +121,7 @@
 							</ul>
 						</article>
 						<article>
-							<a href="#" class="image"><img src="images/pic02.jpg" alt="" /></a>
+							<a href="#" class="image"><img src="images/ОООИжПЗСвязь.jpg" alt="" /></a>
 							<h3>ООО ИжПЗ-Связь</h3>
 							<p>Навигационное оборудование, системы связи, контрольно-проверочная аппаратура, системы оповещения.</p>
 							<ul class="actions">
@@ -129,7 +129,7 @@
 							</ul>
 						</article>
 						<article>
-							<a href="#" class="image"><img src="images/pic03.jpg" alt="" /></a>
+							<a href="#" class="image"><img src="images/ОООИжПЗТест.jpg" alt="" /></a>
 							<h3>ООО ИжПЗ-Тестирование</h3>
 							<p>Испытательный центр: ЭКБ отечественного и иностранного производства; радиоэлектронной аппаратуры и другой готовой продукции.</p>
 							<ul class="actions">
@@ -137,7 +137,7 @@
 							</ul>
 						</article>
 						<article>
-							<a href="#" class="image"><img src="images/pic04.jpg" alt="" /></a>
+							<a href="#" class="image"><img src="images/ОООИжПЗМеханика.jpg" alt="" /></a>
 							<h3>ООО ИжПЗ-Механика</h3>
 							<p>Изделия точной механики, оснастка.</p>
 							<ul class="actions">
@@ -145,7 +145,7 @@
 							</ul>
 						</article>
 						<article>
-							<a href="#" class="image"><img src="images/pic05.jpg" alt="" /></a>
+							<a href="#" class="image"><img src="images/ОООИжПЗФотон.jpg" alt="" /></a>
 							<h3>ООО ИжПЗ-Фотон</h3>
 							<p>Печатные платы, автоматический монтаж элементов.</p>
 							<ul class="actions">
@@ -153,7 +153,7 @@
 							</ul>
 						</article>
 						<article>
-							<a href="#" class="image"><img src="images/pic06.jpg" alt="" /></a>
+							<a href="#" class="image"><img src="images/ОООИжПЗТелеком.jpg" alt="" /></a>
 							<h3>ООО ИжПЗ-Телеком</h3>
 							<p>Технологическая радиосвязь, оборудование для сотовой связи, сенсорика.</p>
 							<ul class="actions">
