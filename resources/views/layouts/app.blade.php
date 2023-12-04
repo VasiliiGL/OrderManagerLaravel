c0a9e2f00fd6cdc9f929688a4e263fc0eccf391f
GasiliiGL <vasiliiglavatskih@gmail.com>
2023-12-04 20:15:53 +0400

    обновление

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
 
 			<!-- Header -->
 				<header id="header">
-					<a href="index.html" class="logo"><strong>Менеджер заказов</strong> ИжПЗ</a>
+					<a href="index.html" class="logo"><strong>Группа компаний</strong> ИжПЗ</a>
 					<ul class="icons">
 						<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
 						<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
