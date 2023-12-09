eaefa727d7ac1d9563d486d52d5482c937af1293
GasiliiGL <vasiliiglavatskih@gmail.com>
2023-12-09 17:42:20 +0400

    удаление неиспользуемых страниц

--- resources/views/partials/posts/item.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="mini-posts">				
-						<h2>{{$post->title}}</h2>
-						<a href="#" class="image"><img src="/storage/posts/{{$post->thumbnail}}" width="200" height="200"alt="" /></a>
-						<p>{!! $post->preview !!}</p>
-				</div>
