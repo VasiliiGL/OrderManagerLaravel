84113d21ce3d1b4bf42d2de86111d107bddf8522
GasiliiGL <vasiliiglavatskih@gmail.com>
2024-02-02 11:52:38 +0400

    Маршруты для авторизации и страницы заказов

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\EditorController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrganizationController;
 use App\Http\Controllers\PostController;
+use App\Models\Organization;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[IndexController::class, 'index'])->name('home');
@@ -27,3 +30,13 @@ Route::get('/store',[EditorController::class, 'store'])->name('store');
 Route::get('/orders',[OrderController::class, 'index'])->name('orders.index');
 // По детальная карточка  заказа
 Route::get('/orders/{id}',[OrderController::class, 'show'])->name('orders.show');
+
+Route::get('/organizations',[OrganizationController::class, 'index'])->name('organizations.index');
+Route::get('/organizations/{id}',[OrganizationController::class, 'show'])->name('organizations.show');
+
+Route::get('/login',[AuthController::class, 'showLoginform'])->name('login');
+Route::get('/logout',[AuthController::class, 'logout'])->name('logout');
+Route::post('/login_process',[AuthController::class, 'login'])->name('login_process');
+
+Route::get('/register',[AuthController::class, 'showRegisterform'])->name('register');
+Route::post('/register_process',[AuthController::class, 'register'])->name('register_process');
