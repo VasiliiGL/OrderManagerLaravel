8e50168b5446557ebbb4d6edf317f2823ba4da44
GasiliiGL <vasiliiglavatskih@gmail.com>
2024-03-02 09:39:58 +0400

    Контроллеры аудентификации для админа и заказчика

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -4,8 +4,39 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class AuthController extends Controller
 {
-    //
+    public function index()
+    {
+        return view("admin.auth.login");
+    }
+
+
+    public function login(Request $request)
+    {
+        $data=$request->validate([
+            "name"=>'required|string|min:4|max:30', //|exists:users,name
+            'password'=>'required|min:4|max:100'
+        ]);
+
+       if (auth("admin")->attempt($data))
+       {
+         //return redirect()->route('admin.posts.index');
+         return redirect()->route('admin.management');
+
+        //return view("admin.management.index");
+       }
+
+        return redirect()->route('admin.login')->withErrors(["name"=>"Пользователь не найден. Проверьте данные"]);
+
+
+    }
+
+    public function logout()
+    {
+        auth("admin")->logout();
+        return redirect()->route('home');
+    }
 }
--- app/Http/Controllers/Customer/AuthController.php
+++ app/Http/Controllers/Customer/AuthController.php
@@ -4,8 +4,38 @@ namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Customer;
 
 class AuthController extends Controller
 {
-    //
+    public function index()
+    {
+        return view("customer.auth.login");
+    }
+
+    public function login(Request $request)
+    {
+        
+        $data=$request->validate([
+            "title"=>'required|string|min:4|max:30', //|exists:users,name
+            'password'=>'required|min:3|max:100'
+        ]);
+      // dd( $data["title"]);
+
+       $idCustomer = Customer::where('title', $data["title"])->value('id');
+       //dd($idCustomer);
+
+       if (auth("customer")->attempt($data))
+       {
+         return redirect()->route('customer.orders.showAll',[ $idCustomer]);
+       }
+
+        return redirect()->route('customer.login')->withErrors(["title"=>"Пользователь не найден. Проверьте данные"]);
+
+    }
+    public function logout()
+    {
+        auth("customer")->logout();
+        return redirect()->route('home');
+    }
 }
