03d4d786d992a8681325255248d5cdea8af6d543
GasiliiGL <vasiliiglavatskih@gmail.com>
2024-03-02 10:11:13 +0400

    страницы регистрации и входа пользователя

--- resources/views/partials/login.blade.php
+++ resources/views/partials/login.blade.php
@@ -1,7 +1,7 @@
 <section>
 <div class="h-screen dg-white flex flex-col space-y-10 justify-center items-center">
     <div class="bg-white w-96 shadow-x1 rounted p-5">
-        <h1 class="text-3x1 font-medium">Вход</h1>
+        <h1 class="text-3x1 font-medium">Вход для сотрудников ИжПЗ</h1>
         <form method="POST" action="{{route('login_process')}}"   class="col-3 offset-4">
             @csrf
             <input name="name" type="text" class="form-control" placeholder="Логин"/>
