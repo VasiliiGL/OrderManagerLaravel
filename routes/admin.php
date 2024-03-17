3d75b4a91b763850c983e589bc07b04cfe0fda0f
Vasilii <vasiliiglavatskih@gmail.com>
2024-03-17 11:48:41 +0400

    Создание ресурсных маршрутов для управления админу заказами, организациями и заказчиками

--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\Admin\AuthController;
 use App\Http\Controllers\Admin\ManagementController;
 use App\Http\Controllers\Admin\PostController;
+use App\Http\Controllers\Admin\OrderController;
+use App\Http\Controllers\Admin\CustomerController;
+use App\Http\Controllers\Admin\OrganizationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('login', [AuthController::class, 'index'])->name('login');
@@ -15,4 +18,10 @@ Route::middleware("guest:admin")->group(function(){
 Route::middleware("auth:admin")->group(function(){
     Route::resource('posts', PostController::class);
     Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+    Route::resource ('orders', OrderController::class);
+    Route::resource ('posts', PostController::class);
+    Route::resource ('customers', CustomerController::class);
+    Route::resource ('organizations', OrganizationController::class);
 });
+
+
