7c6cb72179d3ebc21389e0ce56f7eb4830bd2bd5
VasiliiGL <vasiliiglavatskih@gmail.com>
2024-05-11 17:22:01 +0400

    update progect

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -10,10 +10,9 @@ class IndexController extends Controller
     public function index()
     {
         $user = auth()->user();
-       // return view('home');
+      
         $posts = Post::orderBy("created_at", "DESC")->cursorPaginate(3);
-       //dd($posts);
-       //return view('home');
+  
        return view('home',[
         "posts"=>$posts,"user"=>$user,
        ]);
